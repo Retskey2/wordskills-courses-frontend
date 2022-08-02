f8f8c901d28316a3294cee2ae8adfd142b9b86e3
RETSKEY <bashlovka96@mail.ru>
2022-08-02 23:22:27 +0300

    неправильный импорт (фикс билда)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps } from 'next/app'
 import MainProvider from 'providers/MainProvider'
 
-import Footer from '@/components/ui/Footer/Footer'
-import Header from '@/components/ui/Header/Header'
-import { HeaderContent } from '@/components/ui/Header/header.data'
-
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {

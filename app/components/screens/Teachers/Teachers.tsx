bf4a272fab5c8f945e0f2d85c5fe4799b426d510
Sergey <dungeonmir@gmail.com>
2023-01-07 21:59:10 +0300

    added blue heading and fix for @apply warning

--- app/components/screens/Teachers/Teachers.tsx
+++ app/components/screens/Teachers/Teachers.tsx
@@ -3,12 +3,16 @@ import { FC } from 'react'
 import Heading from '@/components/ui/Heading/Heading'
 import SubHeading from '@/components/ui/Heading/SubHeading'
 import ContactFormTeachers from '@/components/ui/Form/ContactFormTeachers/ContactFormTeachers'
+import HeadingBlock from '@/components/ui/Heading/HeadingBlock'
 
 const Teachers: FC = () => {
 	return (
 		<div className="w-full max-w-[1260px] m-auto mt-12 mb-16">
 			<Heading title="КОМАНДА И ПРЕПОДАВАТЕЛИ" small />
 			<SubHeading>У нас работают лучшие эксперты в своей области</SubHeading>
+
+			<HeadingBlock content="главные эксперты" className='m-16'></HeadingBlock>
+			<HeadingBlock content="Ведущие эксперты" className='m-16'></HeadingBlock>
 			<ContactFormTeachers></ContactFormTeachers>
 
 
---
+++ app/components/ui/Heading/HeadingBlock.tsx
@@ -0,0 +1,36 @@
+import cn from 'classnames'
+import { FC } from 'react'
+
+interface IHeadingBlock {
+    content: string
+    className?: string
+}
+
+const HeadingBlock: FC<IHeadingBlock> = ({ content, className }) => {
+    return (
+        <div className={cn(
+            'w-full',
+            'flex',
+            'justify-center'
+        )}>
+            <div
+                className={cn(
+                    'text-center',
+                    'text-white',
+                    'bg-blue-600',
+                    'text-xl',
+                    'w-[29rem]',
+                    'h-16',
+                    'flex',
+                    'items-center',
+                    'justify-center',
+                    className
+                )}
+            >
+                <p>{content.toUpperCase()}</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeadingBlock

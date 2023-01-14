fc3cfb57b5928d5a9264e58fb31d40cefe037152
Sergey <dungeonmir@gmail.com>
2023-01-14 16:13:07 +0300

    added techersBlocks

--- app/components/screens/Teachers/Teachers.tsx
+++ app/components/screens/Teachers/Teachers.tsx
@@ -4,6 +4,9 @@ import Heading from '@/components/ui/Heading/Heading'
 import SubHeading from '@/components/ui/Heading/SubHeading'
 import ContactFormTeachers from '@/components/ui/Form/ContactFormTeachers/ContactFormTeachers'
 import HeadingBlock from '@/components/ui/Heading/HeadingBlock'
+import TeachersBlock from '@/components/ui/Block/TeachersBlock'
+import TeachersPhoto from '@/assets/image/expert-photo.png'
+import TeachersBlockGrid from '@/components/ui/Block/TeachersBlockGrid'
 
 const Teachers: FC = () => {
 	return (
@@ -11,8 +14,32 @@ const Teachers: FC = () => {
 			<Heading title="КОМАНДА И ПРЕПОДАВАТЕЛИ" small />
 			<SubHeading>У нас работают лучшие эксперты в своей области</SubHeading>
 
-			<HeadingBlock content="главные эксперты" className='m-16'></HeadingBlock>
-			<HeadingBlock content="Ведущие эксперты" className='m-16'></HeadingBlock>
+			<HeadingBlock content="главные эксперты" className='mt-16'></HeadingBlock>
+			<TeachersBlock
+				FIO='Иванов Евгений Витальевич'
+				RoleName='Руководитель проекта'
+				Email='kaspin.dmitrii@mail.ru'
+				Photo={TeachersPhoto}
+				Aligment='Left'
+			/>
+			<TeachersBlock
+				FIO='Иванов Евгений Витальевич'
+				RoleName='Руководитель проекта'
+				Email='kaspin.dmitrii@mail.ru'
+				Photo={TeachersPhoto}
+				Aligment='Right'
+			/>
+			<TeachersBlock
+				FIO='Иванов Евгений Витальевич'
+				RoleName='Руководитель проекта'
+				Email='kaspin.dmitrii@mail.ru'
+				Photo={TeachersPhoto}
+				Aligment='Left'
+			/>
+
+
+			<HeadingBlock content="Ведущие эксперты" className='mt-16'></HeadingBlock>
+
 			<ContactFormTeachers></ContactFormTeachers>
 
 
